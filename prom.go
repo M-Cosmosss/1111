b1bb19eadc823a1ab589214ee44f4331825158f6
M-Cosmosss <yuzhou@koderover.com>
2023-08-18 14:52:47 +0800

    add prom
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

---
+++ prom.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var RequestTotal = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "demo_request_total",
+		Help: "Number of requests",
+	},
+	[]string{"method", "handler", "status"},
+)
+
+func main() {
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(RequestTotal)
+
+	g := gin.Default()
+	g.Use(func(c *gin.Context) {
+		c.Next()
+		RequestTotal.WithLabelValues(c.Request.Method, c.Request.URL.Path, strconv.Itoa(c.Writer.Status())).Inc()
+	})
+	g.GET("/metrics", gin.WrapH(promhttp.HandlerFor(registry, promhttp.HandlerOpts{})))
+	g.GET("/echo/:code", func(c *gin.Context) {
+		code, err := strconv.Atoi(c.Param("code"))
+		if err != nil {
+			c.JSON(400, gin.H{
+				"message": "bad request",
+			})
+			return
+		}
+		c.JSON(code, gin.H{
+			"message": "hello world",
+		})
+		return
+	})
+	fmt.Println(g.Run(":8080"))
+}
